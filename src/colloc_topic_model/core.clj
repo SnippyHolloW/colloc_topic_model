6daa75b7749672448cb1e8b04acf840660110d80
Benjamin Börschinger <benjamin.boerschinger@googlemail.com>
2013-11-26 22:59:48 +0100

    initialize-colloc-topics works, but need to fix initialization for parse-uts, and haven't restructured parse-uts

--- src/colloc_topic_model/core.clj
+++ src/colloc_topic_model/core.clj
@@ -3,7 +3,7 @@
         [clojure.string :only (join split replace-first)]))
 
 (defn read-corpus []
-  (with-open [rdr (reader "resources/nipscollocation.yld")]
+  (with-open [rdr (reader "resources/nipscollocation_1000.yld")]
     (let [lines (doall (line-seq rdr))]
       (reduce (partial merge-with #(conj %1 (first %2)))
               (map (fn [x] {(first x) [(second x)]})
@@ -21,39 +21,58 @@
                               (map (partial initialize-doc n-topics) 
                                    (second id-doc))]) corpus)))
 
-(defn initialize-colloc-topics [n-topics alpha corpus z]
+
+(defn parse-ut [buffer accs words_indicators]
+  (let [words (first words_indicators)
+        indicators (second words_indicators)]
+    (if (or (= words nil) (= words '()))
+      accs
+      (let [word (first words)
+            ind  (first indicators)
+            remainder [(rest words) (rest indicators)]]
+        (if (= ind 0)
+          (parse-ut (conj buffer word) accs remainder)
+          (parse-ut [] 
+                    (let 
+                        [topic-ind (- ind 1)
+                         acc (get accs topic-ind)
+                         colloc (conj buffer word)
+                         new-count (+ (get acc colloc 0) 1)]
+                      (assoc accs topic-ind
+                             (assoc acc colloc new-count)))
+                    remainder))))))
+
+
+(defn merge-hm-vecs [l1 l2]
+  (if (= l1 '())
+      '()
+      (let [l1r (rest l1)
+            l2r (rest l2)
+            hm1 (first l1)
+            hm2 (first l2)]
+           (cons (merge-with #(+ %1 %2) hm1 hm2)
+                 (merge-hm-vecs l1r l2r)))))
+
+
+
+(defn initialize-colloc-topics [n-topics corpus z]
+  (reduce merge-hm-vecs 
+          ;; TODO --- initialize accs properly...
+          (map (partial parse-ut [] [{} {} {} {}])
+               (reduce (fn[x y] (conj (second x) (second y)))
+                       (merge-with #(zipmap %1 %2) corpus z)))))
+
   ;(vec (repeat n-topics (map (fn [doc-w doc-z] (map (fn [sent-w sent-z] ))) corpus z))))
   ;TODO
-  )
+;  )
 
-(defn parse-ut [words indicators buffer accs]
-  (if (= words '())
-    accs
-    (let [ word (first words)
-         ind  (first indicators)]
-         (if (= ind 0)
-           (parse-ut (rest words) (rest indicators) (conj buffer word) accs)
-           (parse-ut (rest words) (rest indicators) [] 
-               (let 
-                 [acc (get accs ind-1)
-                  colloc (conj buffer word)
-                  new-count (+ (get acc colloc 0) 1)]
-                 (assoc accs ind
-                        (assoc acc word new-count))
-                )
-            )
-         )
-    )
-  )
-)
-
-
-
+;combine individual utt-parse-hms
 
 (defn -main [& args]
-  (let [n-topics 10
+  (let [n-topics 4
         corpus (read-corpus)
         z (random-init-topic-boundaries corpus n-topics)
-        phi-k (initialize-colloc-topics n-topics alpha corpus z)]
-    (prn z))
-  )
+        phi-k (initialize-colloc-topics n-topics corpus z)]
+;    (prn z))
+    (prn phi-k))
+ )
